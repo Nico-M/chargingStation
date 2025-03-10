648ecd87727bcdec8b2e9efbbfc0405d7d2bb5c4
Nico <531521711@qq.com>
2025-03-10 12:52:19 +0800

    Update base path in Vite configuration to '/chargingStation'

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ import svgLoader from "vite-svg-loader";
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/yilaidian",
+  base: "/chargingStation",
   plugins: [vue(), UnoCSS(), svgLoader()],
   resolve: {
     alias: {
